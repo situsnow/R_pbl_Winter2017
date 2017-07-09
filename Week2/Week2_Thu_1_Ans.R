ad1ea1c2994feb2606844b7cd09d22b14d6552c1
situsnow <situsnow@163.com>
2017-07-09 14:12:26 +1000

    Update Week2 Thursday tutorial

---
+++ Week2/Week2_Thu_1_Ans.R
@@ -0,0 +1,137 @@
+---
+  Title: "Module 1  Introduction to R - Week 2 Session 1 Answers."
+Author:"Doogan, C.(2017)."
+
+Date:  "July 2017."
+---
+  # This week we will be focusing on basic data exploration. However, we first need to learn about control flow. 
+  # This is fundamental to most programming languages and allows us to make conditions. 
+  # Since we have packages such as dplr though, we don't need to know any more than the basics.
+  # We will cover control structures in session 2 this week.
+  
+  # Install packages
+  install.packages("dyplr")
+library(dplyr)
+
+# Import data
+gapminder <- read.csv("gapminder-FiveYearData.csv", header = TRUE)
+
+### select()
+# We use select when we only want to work with a few of the variables in our dataset. 
+year_country_gdp <- select(gapminder,year,country,gdpPercap)
+
+# Having a look shows that we only have the year, the country and the GDP as a percentage.
+head(year_country_gdp)
+
+# Challenge 1: We wrote this code without using any pipes (%>%). Why should we consider using pipes?
+# Answer:
+# Pipes allow us to save time writting (and reduce syntax errors), aid R in running large data sets faster and imporve the readability of the code. 
+
+# This is the same code but with pipes.
+year_country_gdp <- gapminder %>% select(year,country,gdpPercap)
+
+# What did we just write?
+# Challenge 2: Translate the above code with pipes into English step-by-step.
+# Answer:
+# Take the gapminder data set and then pipe it to call the select function on it so that only the 
+# year, country and GDP per capita are going to be stored in a new variable called year_country_gdp.
+
+
+### filter()
+# We use filter() when we want to do something with only a portion of the data that meets a condition.
+# Filter is similar to our conditional statements.
+# In this example we only want to get data from our newly selected data frame, that is from continental Europe.
+year_country_gdp_euro <- gapminder %>% filter(continent=="Europe") %>% select(year,country,gdpPercap)
+
+# Challenge 3: Write a code that will produce a dataframe that has 
+# the values for lifeExp, country and year for African countires only.
+# How many rows does your dataframe have and why?
+# Answer:
+year_country_lifeExp_Africa <- gapminder %>% filter(continent=="Africa") %>% select(year,country,lifeExp)
+
+# Here we have used pipes to pass the gapminder data frame to the filter() function. Then we passed the filtered gapminder 
+# data frame to the select() function.
+The order of functions here is important. 
+#Try swapping the filter() and select() functions around in the above code.
+# What is the result? Why has this happened?
+# Answer:
+year_country_gdp_africa <- gapminder %>%select(year,country,gdpPercap) %>%filter(continent=="Africa")
+# This causes a error. 
+
+### group_by() and summarize()
+
+# One of the reasons we use dplyr is because it reduces the repetitiveness of writing code.
+# This helps reduced the number of errors we may encounter (because of our typing)
+# Let's say we wanted to find out year, countries and life expectancy for each of the continents. 
+# This is how we would do that without writing out the same code over and over.
+# We don't use filter() to get observations as it would only get the observations
+# that meet certain criteria e.g.continent =="Europe".
+# Instead we use the group_by() function which allows us to use all the unique criteria that we did in filter(). 
+# We make use of str()
+str(gapminder)
+str(gapminder %>% group_by(continent))
+head(gapminder)
+
+# Not much happens with the group_by() function unless it is paired with the summarise function.
+# We split our gampminder data frame up using the group_by() function. We now have groups of data for each continent.
+# Using summarise(), we can now pass data from the new continent data frames to more functions, e.g., mean, median, etc.
+# In this next example, we want to find the mean GDP per capita for each of the continents.
+gdp_bycontinents <- gapminder %>% group_by(continent) %>% summarize(mean_gdpPercap=mean(gdpPercap))
+print(gdp_bycontinents)
+
+# Challenge 4: Calculate the average life expectancy per country. 
+# Which has the longest average life expectancy and which has the shortest average life expectancy?
+# Answer:
+lifeExp_bycountry <- gapminder %>% group_by(country) %>% summarize(mean_lifeExp=mean(lifeExp))
+lifeExp_bycountry %>% filter(mean_lifeExp == min(mean_lifeExp) | mean_lifeExp == max(mean_lifeExp))
+# Iceland 76.51 Year
+# Sierra Leone 36.77 Years
+
+### arrange()
+# We could do this challenge another way. That is to use dplyrs arrange() function. Using this function, we can
+# arrange the rows in a data frame according to the order of any of the variables in the data frame. We can choose more than 
+# one variable to arrange the data frame by. It is similar to the sort button in Excel. 
+# You can use desc() inside of arrange() arrange(desc()) to order the data in descending order.
+lifeExp_bycountry %>% arrange(mean_lifeExp) %>%
+  head(1)
+
+# Challenge 5:Rearrange the lifeExp_bycountry in descending order.
+# Answer:
+lifeExp_bycountry %>% arrange(desc(mean_lifeExp)) %>%
+  head(1)
+
+# Using group_by() for multiple variables.
+# We can use group_by form more than one variable. In this example we will group by year and continent. 
+gdp_bycontinents_byyear <- gapminder %>% group_by(continent,year) %>% summarize(mean_gdpPercap=mean(gdpPercap))
+head(gdp_bycontinents_byyear)
+
+# The advantage of using group_by() is that you can now call functions on multiple variables using summarize.
+
+# Challenge 6: In Challenge 4 you calculated the life expectancy per country.
+# Now calculate the GDP by year for each continent and provide the mean and standard deviation *sd()* GDP per capita, 
+# and the mean and standard deviation of the population. 
+# Answer
+gdp_pop_bycontinents_byyear <- gapminder %>% group_by(continent,year) %>% summarize(mean_gdpPercap=mean(gdpPercap),sd_gdpPercap=sd(gdpPercap),mean_pop=mean(pop),sd_pop=sd(pop))
+
+### Count() and nd()
+# You will be asked to count some observations a lot. dThere are two dplyr functions to do this. 
+# Count() is similar to a conditional statement. It will take the specification and match it to all of the data in a column
+# The then gives you how many of those observations matched your specification. 
+# You can also sort the data at the same time using sort =TRUE.
+gapminder %>% filter(year == 2002) %>% count(continent, sort = TRUE)
+
+# If we wanted to know the number of observations in calculations then we can use n().
+#In this example we want to know the standard error of the life expectancy per continent. 
+gapminder %>% group_by(continent) %>% summarize(se_pop = sd(lifeExp)/sqrt(n()))
+
+### Mutate()
+# We can also create new variables prior to (or even after) summarizing information using mutate().
+gdp_pop_bycontinents_byyear <- gapminder %>% mutate(gdp_billion=gdpPercap*pop/10^9) %>% group_by(continent,year) %>% summarize(mean_gdpPercap=mean(gdpPercap), sd_gdpPercap=sd(gdpPercap), mean_pop=mean(pop), sd_pop=sd(pop), mean_gdp_billion=mean(gdp_billion), sd_gdp_billion=sd(gdp_billion))
+head(gdp_pop_bycontinents_byyear)
+
+# A lot of what we have covered is relatively advanced in respect to your MDS units, 
+# but they are powerful, and we will revisit their use later.
+
+# End Session.
+
+# References:Software Carpentry https://swcarpentry.github.io/r-novice-gapminder/13-dplyr/
---
+++ Week2/Week2_Thu_2_Ans.R
@@ -0,0 +1,208 @@
+---
+  Title: "Module 1  Introduction to R - WeeK 2 Session 2b Answers."
+Author:"Doogan, C, Gao, C. (2017)."
+
+Date:  "July 2017."
+---
+  
+  # Humans are visual creatures, and it is much easier to see a graphical representation when working with data than to post a bunch of numbers on a screen.
+  # For this reason we are going to be learning how to visualise data in R at the same time as conducting an exploratory statistical analysis. 
+  # ggplot2 is the later in R visualisation packaging. It is far more superior than older scripting methods, and you should adopt this package as your go-to rather than other methods. 
+  # Although ggplot2 is fantastic for in-depth visualisation, some of the more basic methods are fine to use when starting off. ggplot2 incorporates these in-built functions you will see. 
+  # *Note* some MDS units do not teach ggplot2 but accept theIR use in assessments  (and tutors even enjoy looking at them more!).However, as some markers may not be as familiar with
+  # ggplot2 as other methods, you should make sure that your comments are concise but helpful in explaining your rationale. 
+  
+  
+  # Install packages
+  install.packages("ggplot2")
+library(ggplot2)
+library(dplyr)
+
+# Import data
+getwd()
+
+gapminder <- read.csv("gapminder-FiveYearData.csv", header = TRUE)
+
+### Base Plotting
+# Base R provide convince ways to quickly generate graphics. The primary functions include:
+# Here we will use the mtcars dataset to have a look at these functions. 
+# First we need to see what variables we have so we call dim() and summary()
+dim(mtcars)
+# This is a data frame with 32 observations on 11 variables.
+# The search function of the Help tab in the Environment window is used to find out what these variables mean.
+# *Note* In some MDS units, explanations of what the variables are may not be given. You must assess the features of this data to get an understanding of how to manipulate it.
+# This is a key aspect of working with data in data science.
+# From the help function we learn that the variables in mtcars are:
+# [, 1]     mpg     Miles/(US) gallon
+# [, 2]     cyl     Number of cylinders
+# [, 3]     disp     Displacement (cu.in.)
+# [, 4]     hp     Gross horsepower
+# [, 5]     drat     Rear axle ratio
+# [, 6]     wt     Weight (1000 lbs)
+# [, 7]     qsec     1/4 mile time
+# [, 8]     vs     V/S
+# [, 9]     am     Transmission (0 = automatic, 1 = manual)
+# [,10]     gear     Number of forward gears
+# [,11]     carb     Number of carburettors
+
+# Challenge 1: Recall what [,1] means.
+# Answer: 
+# This means the data frame arranges each of the variables by volume. The mpg variable is stored in the first column. 
+
+# This is observed when we call the summary() function. 
+summary(mtcars)
+# Using summary we see that the data is quite varied. Our first goal is to determine what type of data this is, e.g., continuous, discrete, categorical, binary, etc.
+# For an overview of the statistical data type.
+# We see that there is a lot of what looks like numerical continuous (mpg, disp, drat, wt, qsec), numerical discrete (cyl, hp, gear), categorical nominal (vs, am).
+
+# To make sure we are right though. We can inspect using the pairs() function:
+pairs(mtcars)
+
+# plot: make scatter plot
+# We investigate the gears variable which we are not sure is categorical or numerical
+plot(mtcars$gear)
+# We should do this as a frequency
+tab_gears<-table(mtcars$gear)
+plot(tab_gears)
+
+# It is probably discrete, but we cannot be sure.
+# The rest of the functions are:
+
+# lines: add lines to plot
+# points: add points to plot
+# text: add text
+# title: add title to axes or plot
+# mtext: add margin text
+# axis: add axis tick/labels
+# hist: make a histogram
+# box: make a histogram
+# Using these functions, plotting features can be specified using following parameters:
+# pch: plotting symbol
+# lty: line type
+# lwd: line width
+# col: plotting colour
+# las: orientation of axis labels
+# bg: background color
+# mar: margin size
+# oma: outer margin size
+# mfrow: number of plots per row, column. Plots filled in row-wise.
+# mfcol: number of plots per row, column. Plots filled in column-wise.
+# xlab: a title for the x axis
+# ylab: a title for the y axis: see title.
+
+# Working on gapminder:
+
+### Plotting : plot()
+# Look at the life expectancy 
+plot (gapminder$lifeExp)
+# It is very difficult to look at and infer anything about the data. We can present
+# the data by year and we will only look at the mean.
+life_exp_year<-gapminder %>% group_by (year) %>% summarise(mean_lifeExp=mean(lifeExp))
+plot(life_exp_year)
+# Add some lables
+plot(life_exp_year, xlab="Year", ylab="Life expectancy")
+
+
+# Boxplots: box()
+# making a boxplot with GDP
+boxplot(gapminder$gdpPercap, ylab="GDP per capita")
+# That looks horrible. We can fiX it using boxwex= a scaling factor to make boxes narrower or wider and log= to scale . 
+boxplot(gapminder$gdpPercap, ylab="GDP per capita", boxwex =1,log = "y" )
+# Change the colour of the boxplot with col= and the border with border=
+boxplot(gapminder$gdpPercap, ylab="GDP per capita", boxwex =1,log = "y", col="red", border="Blue" )
+
+# We used group_by() to get the mean using the summarize() functions. But a simple way when just wanting to 
+# display data by another variable is to use ~ e.g. gdpPercap ~ year
+# Challenge 2: Create a boxplot of life expectancy by continent
+# Answer:
+boxplot(lifeExp~continent, data=gapminder, ylab="Life expectancy")
+
+### ggplot2 
+# All of those functions can be called on different graph types
+# e.g plot(), hist(), bar() etc. But ggplot2 can be thought of as a way to use those graph types with add ons to 
+# make the functions look more visually pleasing.
+# ggplot2 is an advanced plotting package, which is increased due to its efficiency and flexibility.
+#here does the �gg� in ggplot2 come from? The ggplot2 package provides an R implementation of Leland Wilkinson�s Grammar of Graphics (1999).
+# The Grammar of Graphics allows you to think beyond the garden variety plot types (e.g. scatter plot, bar plot) and the 
+# consider the components that make up a plot or graphic, such as how data are represented on the plot (as lines, points, etc.),
+# how variables are mapped to coordinates or plotting shape or color, what transformation or statistical summary is required,
+# and so on.the idea that any plot can be expressed from the same set of components: 
+# a data set, a coordinate system, and a set of geoms�the visual representation of data points.
+# The key to understanding ggplot2 is thinking about a figure in layers.
+# This idea may be familiar to you if you have used image editing programs like Photoshop, Illustrator, or Inkscape.
+
+# Specifically, ggplot2 allows you to build a plot layer-by-layer by specifying:
+# a geom, which specifies how the data are represented on the plot (points, lines, bars, etc.),
+# aesthetics that map variables in the data to axes on the plot or plotting size, shape, colour, etc.,
+# a stat, a statistical transformation or summary of the data applied before plotting,
+# facets, which we�ve already seen above that allow the data to be divided into chunks by other categorical 
+# or continuous variables and the same plot drawn for each piece.
+
+# The commonly used plotting functions include:
+# geom_point: for scatter plot
+# geom_line: for line plot.
+# geom_bar: for bar plot
+# geom_histogram: for histogram
+# geom_boxplot: for boxplot
+# geom_density: for smoothed density plot
+
+# If we want to plit a continous variable X by continous variable Y we can use a scatterplot.
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point()
+# We are telling ggplot2 to go and user the gapminder dataset
+# Then use aesthetic mapping to map the gdpPerCap data onto the x axis and the lifeExp onto the y axis. 
+# We use geom_point() to to specificy the geometric object that needs to be ploted (there are many types).
+# We can add lables and then change the size of the points using size().
+# We can also modify the transparency of the points, using the alpha function,
+# which is especially helpful when you have a large amount of data which is very clustered.
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point( size = 2,alpha = 0.5)+  xlab("GDP per capita")+ ylab("Life expectancy")
+
+# It is hard to see what is happening so we can add some colour. Here we tell ggplot to plot
+# the data by continent
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) +
+  geom_point( size = 2,alpha = 0.5)+
+  xlab("GDP per capita")+
+  ylab("Life expectancy")
+# It is still hard to see what is happening because of all the ouliers.
+# So now we can change the scale of the data using the scale_log_10() function.
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) +
+  geom_point( size = 2,alpha = 0.5)+
+  xlab("GDP per capita")+
+  ylab("Life expectancy")+
+  scale_x_log10()
+# The log10 function applied a transformation to the values of the gdpPercap column before rendering them on the plot,
+# so that each multiple of 10 now only corresponds to an increase in 1 on the transformed scale,
+# e.g. a GDP per capita of 1,000 is now 3 on the y axis, a value of 10,000 corresponds to 4 on the y axis and so on.
+# This makes it easier to visualize the spread of data on the x-axis.
+
+# Challenge 2: Create a density plot of GDP per capita, 
+# filled by continent.Transform the x axis to better visualise the data spread.
+#  Answer:
+ggplot(data = gapminder, aes(x = gdpPercap, fill=continent)) +
+  geom_density(alpha=0.6) +
+  xlab("GDP per capita")+
+  ylab("Density")+
+  scale_x_log10()
+#  This is very helpful for comparing distributions.
+
+# plot() and pairs() is used to see multi-pannel graphs of variables.
+pairs(gapminder)
+plot(gapminder$lifeExp,gapminder$country)
+# But ggplot2 has a nicer way:
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point( size = 1,alpha = 0.5)+
+  xlab("GDP per capita")+
+  ylab("Life expectancy") +
+  facet_wrap( ~ continent) +
+  scale_x_log10()
+
+# Challange 3:Add a facet layer to panel the previous density plots by year.
+# Answer:
+ggplot(data = gapminder, aes(x = gdpPercap, fill=continent)) +
+  geom_density(alpha=0.6) +
+  xlab("GDP per capita")+
+  ylab("Density")+
+  scale_x_log10() +
+  # your code:
+  facet_wrap( ~ year) 
+
+# End session. 
